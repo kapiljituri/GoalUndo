5badb1cd61da73952cdf50819bb0886221dbed21
kapiljituri <kapiljituri@gmail.com>
2016-05-17 07:45:58 -0700

    Added 3 test cases: EmptyCheck, NotEmptyCheck and MultipleOperationsInSingleGoal

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
- 
+
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -14,7 +14,37 @@ class GoalUndoTest : public ::testing::Test
 		virtual void TearDown(){}
 };
 
-TEST(GoalUndoTest, sanityCheck)
+TEST(GoalUndoTest, EmptyCheck)
 {
-	ASSERT_TRUE(true);
+  GoalUndo task;
+	ASSERT_EQ("", task.getGoal());
+}
+
+TEST(GoalUndoTest, NotEmptyCheck)
+{
+  GoalUndo task;
+  task.addOperation("gFirst", "oFirst");
+	ASSERT_EQ("gFirst", task.getGoal());
+}
+
+TEST(GoalUndoTest, MultipleOperationsInSingleGoal)
+{
+  int i;
+  GoalUndo task;
+  // Adding first operation in first goal
+  task.addOperation("gFirst", "myOperation");
+
+  // Adding 20 more operations in single goal (total = 21)
+  for(i=0; i<20; i++){
+    task.addOperation("myOperation");
+  }
+
+  // Removing 19 operations (remaining = 2)
+  for(i=0; i<19; i++){
+    task.undoOperation();
+  }
+
+  // Asserting if 2 operations present
+  ASSERT_EQ("myOperation myOperation", task.getOperations());
+
 }
