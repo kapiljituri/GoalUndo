52478748d80b3a8fd6d0ff3fb7d12825fb708018
kapiljituri <kapiljituri@gmail.com>
2016-05-17 09:19:35 -0700

    3 more tests

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -44,7 +44,68 @@ TEST(GoalUndoTest, MultipleOperationsInSingleGoal)
     task.undoOperation();
   }
 
-  // Asserting if 2 operations present
+  // Asserting if 2 operations are present
   ASSERT_EQ("myOperation myOperation", task.getOperations());
+}
 
+TEST(GoalUndoTest, RemovingOperationsInRandomOrder)
+{
+  GoalUndo task;
+  task.addOperation("gFirst", "oFirst");
+  task.addOperation("oSecond");
+  task.addOperation("oThird");
+  task.addOperation("oFourth");
+  task.addOperation("oFirst"); // Duplicate operation
+  task.addOperation("oFifth");
+
+	ASSERT_EQ("oFirst oSecond oThird oFourth oFirst oFifth", task.getOperations());
+
+  task.undoOperation("oThird");
+  ASSERT_EQ("oFirst oSecond oFourth oFirst oFifth", task.getOperations());
+
+  // Removing duplicate 'oFirst' operation
+  task.undoOperation("oFirst");
+  ASSERT_EQ("oFirst oSecond oFourth oFifth", task.getOperations());
+
+  // Removing non existing operation
+  task.undoOperation("oSixth");
+  ASSERT_EQ("oFirst oSecond oFourth oFifth", task.getOperations());
+
+  task.undoOperation("oFourth");
+  task.undoOperation("oFirst");
+  task.undoOperation("oFifth");
+  ASSERT_EQ("oSecond", task.getOperations());
+
+  task.undoOperation("oSecond");
+  ASSERT_EQ("", task.getOperations());
+}
+
+TEST(GoalUndoTest, DefaultUndoOperationBehavior)
+{
+  int i;
+  GoalUndo task;
+  task.addOperation("myGoal", "myOperation");
+  // Adding 9 more operations in single goal (total = 10)
+  for(i=0; i<9; i++){
+    task.addOperation("myOperation");
+  }
+
+  // Removing 19 operations (remaining = 2)
+  for(i=0; i<10; i++){
+    task.undoOperation();
+  }
+
+	ASSERT_EQ("", task.getGoal());
+}
+
+TEST(GoalUndoTest, OverloadedUndoOperationBehavior)
+{
+  GoalUndo task;
+  task.addOperation("gFirst", "oFirst");
+  task.addOperation("oSecond");
+
+  task.undoOperation("oFirst");
+  task.undoOperation("oSecond");
+
+	ASSERT_EQ("", task.getGoal());
 }
